a092a7a2f7ed38d3dba01f19ea0993a2cca8ec54
KaitoMuraoka <kaitoboshi@icloud.com>
2024-01-10 21:43:37 +0900

    create Lesson4 App

---
+++ MyMusic/MyMusic/MyMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMusicApp.swift
+//  MyMusic
+//
+//  Created by 村岡海人 on 2024/01/10.
+//
+
+import SwiftUI
+
+@main
+struct MyMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
